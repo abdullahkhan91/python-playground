d565a08cf1492ae3bcb528bca64243fc21e5cd1d
abdullahkhan91 <abdullah.ali91@gmail.com>
2020-01-13 15:58:45 -0500

    add comments to python script

--- readwrite.py
+++ readwrite.py
@@ -2,6 +2,7 @@
 with open('name.txt') as f:
     full_name = f.read()
 
+#opens a text file and appends string to the variable from above
 with open('hello.txt', 'w') as g:
     g.write('hello, my name is ')
     g.write(full_name)
